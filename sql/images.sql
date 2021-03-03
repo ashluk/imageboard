2e794b8e29ac57bc5410f59d848ce401d14cf770
Ash Gowriluk <ashleerluk@gmail.com>
2021-03-03 01:16:39 +0100

    1:16am

--- sql/images.sql
+++ sql/images.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS comments;
+
 
 CREATE TABLE images(
     id SERIAL PRIMARY KEY,
@@ -6,6 +8,13 @@ CREATE TABLE images(
     username VARCHAR NOT NULL,
     title VARCHAR NOT NULL,
     description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    imageid INT REFERENCES images(id),
+    comment VARCHAR NOT NULL,
+    username VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
